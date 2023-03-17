ba23db21f12502e69b061dbf20b88613dc89addc
harishmithak <kharishmitha@gmail.com>
2023-03-17 09:42:35 +0530

    FINAL COMMIT

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function validation() {
     var name = document.register.name.value;
     var dob = document.register.dob.value;
     var gender = document.register.gender.value;
-    var fname = document.register.fathername.value;
+   var fathername = document.register.fathername.value;
     var address = document.register.address.value;
     var mobile = document.register.mobileno.value;
     var phone = document.register.phoneno.value;
@@ -12,11 +12,11 @@ function validation() {
     var mark = document.register.mark.value;
     var interest = document.register.Interest.value;
 
-    var markreg=/^[0-9]{3}.[0-9]{2}$/
-    var phonereg=/^[0-9]{2}-[0-9]{3}-[0-9]{6}$/
-    var mobilereg=/^[0-9]{10}$/
-    var namereg = /^[A-Za-z]+$/
-    var emailreg = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
+    var markreg=/^[0-9]{3}.[0-9]{2}$/;
+    var phonereg=/^[0-9]{2}-[0-9]{3}-[0-9]{6}$/;
+    var mobilereg=/^[0-9]{10}$/;
+    var namereg = /^[A-Za-z]+$/;
+    var emailreg = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
     if (name == '') {
         alert('Please enter your name');
@@ -38,37 +38,39 @@ function validation() {
     }
      else {
         alert('Thank you for registering');
-        display(name, dob, gender, fathername, address, mobile, phone, email, college, mark, interest);
+        document.write("<h2>Registration Details:</h2>");
+        document.write("<p>Name: " + name + "</p>");
+        document.write("<p>Date of Birth:" + dob + "</p>");
+        document.write("<p>Gender: " + gender + "</p>");
+        document.write("<p>Father's Name:" + fathername + "</p>");
+        document.write("<p>Address:" + address + "</p>");
+        document.write("Mobile Number" + mobile + "</p>");
+        document.write("<p>Phone Number:" + phone + "</p>");
+        document.write("<p>Email: " + email + "</p>");
+        document.write("<p>College Name: " + college + "</p>");
+        document.write("<p>Mark: " + mark + "</p>");
+        document.write("<p>personal interest:"+interest+"</p>");
+    if (dancing) {
+    document.write("<p>dancing</p>");
     }
-}
+    if (singing) {
+    document.write("<p>singing</p>");
+    }
+    if (travelling) {
+    document.write("<p>travellingi</p>");
+    }
+    if (acting) {
+    document.write("<p>acting</p>");
+    }
+    
+        // display(name, dob, gender, fathername, address, mobile, phone, email, college, mark, interest);
+    }
 
 
 
-function display(name, dob, gender, fname, address, mobile, phone, email, college, mark, interest) {
-    document.write("<h2>Registration Details:</h2>");
-    document.write("<p>Name: " + name + "</p>");
-    document.write("<p>Date of Birth:" + dob + "</p>");
-    document.write("<p>Gender: " + gender + "</p>");
-    document.write("<p>Father's Name:" + fname + "</p>");
-    document.write("<p>Address:" + address + "</p>");
-    document.write("Mobile Number" + mobile + "</p>");
-    document.write("<p>Phone Number:" + phone + "</p>");
-    document.write("<p>Email: " + email + "</p>");
-    document.write("<p>College Name: " + college + "</p>");
-    document.write("<p>Mark: " + mark + "</p>");
-    document.write("<p>personal interest:</p>");
-if (dancing) {
-document.write("<p>dancing</p>");
-}
-if (singing) {
-document.write("<p>singing</p>");
-}
-if (travelling) {
-document.write("<p>travellingi</p>");
-}
-if (acting) {
-document.write("<p>acting</p>");
-}
 
+// function display(name, dob, gender, fathername, address, mobile, phone, email, college, mark, interest) {
+   
     
 }
+
